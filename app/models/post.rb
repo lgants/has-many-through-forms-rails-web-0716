7df9e66295715461a2df369ac86774f45afbe747
lgants <lgants@gmail.com>
2016-08-23 17:06:16 -0400

    has many through lab complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,26 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  # below doesn't crossreference to check for prexisting categories with same name
+  # commented out so that we can use our custom method below
+  # accepts_nested_attributes_for :categories
 
 
+  # custom method to crossreference for prexisting categories with same name
   def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.post_categories.build(category: category)
-    end
+      category_attributes.values.each do |category_attribute|
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
   end
+
+  # accepts_nested_attributes_for :categories <- makes code below unnesecary
+  # below doesn't crossreference to check for prexisting categories with same name
+  # def categories_attributes=(category_attributes)
+  #   category_attributes.values.each do |category_attribute|
+  #     category = Category.find_or_create_by(category_attribute)
+  #     self.post_categories.build(category: category)
+  #   end
+  # end
 
 end
